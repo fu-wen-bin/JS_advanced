f3acabfad0efe35b94fd85bc17cd6ab4ac7a7ace
文斌 傅 <fwb2580@outlook.com>
2025-05-05 23:25:10 +0800

    JS进阶

--- Day01/13-多级对象解构.js
+++ Day01/13-多级对象解构.js
@@ -26,11 +26,31 @@ const pig2 = [
       brother2: '乔治',
     },
     age2: 6,
-  }]
+  },
+  {
+    name3: '乔治',
+    family3: {
+      mother3: '猪妈妈',
+      father3: '猪爸爸',
+      sister: '佩奇',
+    },
+    age3: 3,
+  },
+]
 
 // 多级解构
-const [{ name2, family2: { mother2, father2, brother2 } }] = pig2
+const [
+  { name2, family2: { mother2, father2, brother2 }, age2 },
+  { name3, family3: { mother3, father3, sister }, age3 },
+] = pig2
 console.log(name2)
 console.log(mother2)
 console.log(father2)
 console.log(brother2)
+console.log(age2)
+console.log('----------------')
+console.log(name3)
+console.log(mother3)
+console.log(father3)
+console.log(sister)
+console.log(age3)
